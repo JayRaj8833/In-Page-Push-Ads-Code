78401b3c2bf9240b95901162686a71d96ce482d2
JayRaj ❤️ <87997138+JayRaj8833@users.noreply.github.com>
2024-10-08 00:29:18 +0530

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,28 @@
+<center>
+            <script type='text/javascript'>
+                google_ad_client = "pub-1433987435059783";
+                google_ad_host = "pub-8726151893822553";
+                google_ad_width = 300;
+                google_ad_height = 250;
+                google_ad_type = "text_image";
+                google_color_border = "FFFFFF";
+                google_color_bg = "ffffff";
+                google_color_link = "0000ff";
+                google_color_text = "000000";
+                google_color_url = "008000";
+                google_page_url = "https://yo.fan/";
+            </script>
+            <script src='https://pagead2.googlesyndication.com/pagead/show_ads.js' type='text/javascript'></script>
+          <div style="
+  text-align: right;
+  font-size: 10px;
+  margin-top: 5px;
+  color: #000000;">
+  Ads by <a href="https://alphaadcodegenerator.blogspot.com/" target="_blank" style="
+    color: #0000ff;
+    text-decoration: none;">
+    Alpha Ad
+  </a>
+</div>
+
+      </center>
